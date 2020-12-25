7fc74da6369303626f9aa9bb1f6c714ae3127eff
xwfh2000 <xwfh2009@yahoo.com>
2020-12-25 12:40:49 +0800

    将地图的基本存储单位改为二维数组，内容为结构体，结构体内目前只有一个字符串类型的groupNum.

--- DrawSubRegionPolyomino/Form1.cs
+++ DrawSubRegionPolyomino/Form1.cs
@@ -18,7 +18,7 @@ namespace DrawSubRegionPolyomino
 
         private void Form1_Load(object sender, EventArgs e)
         {
-           
+
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -28,35 +28,33 @@ namespace DrawSubRegionPolyomino
             double roundratio = 0.5f;
             float penWidth = 1;
             List<Point> pts = new List<Point>();
-            List<string> groups = new List<string>();
+            Piece[,] pieces = new Piece[10, 10];
             foreach (Control item in panel1.Controls)
             {
                 //判断是为TextBox框
-                if (item is TextBox&&item.Text!="")
+                if (item is TextBox && item.Text != "")
                 {
                     string name = item.Name;
-                    int textnum = Convert.ToInt32(name.Substring(7,name.Length-7))-1;
-                    pts.Add(new Point(textnum%10,textnum/10));
-                    groups.Add(item.Text);
+                    int textnum = Convert.ToInt32(name.Substring(7, name.Length - 7)) - 1;
+                    pieces[textnum % 10, textnum / 10].groupNUm = item.Text;
                 }
             }
 
-            DrawPoly(new Pen(Color.Black, penWidth), gp, gridsize, 
-                pts.ToArray(),groups.ToArray(), roundratio);
+            DrawPoly(new Pen(Color.Black, penWidth), gp, gridsize, pieces, roundratio);
             gp.Dispose();
         }
         private void DrawL(Pen pen, Graphics gp, int X1, int Y1, int X2, int Y2)
         {
-            gp.DrawLine(pen, new Point(X1, Y1), new Point(X2,Y2));
+            gp.DrawLine(pen, new Point(X1, Y1), new Point(X2, Y2));
         }
 
-        private void DrawLeftLine(Pen pen,Graphics gp, double gridsize, int X,int Y, int radius)
+        private void DrawLeftLine(Pen pen, Graphics gp, double gridsize, int X, int Y, int radius)
         {
             gp.DrawLine(pen, X, Y, X + radius, Y);
         }
         private void DrawMidLine(Pen pen, Graphics gp, double gridsize, int X, int Y, int radius)
         {
-            gp.DrawLine(pen, X+radius, Y, X +(int)gridsize- radius, Y);
+            gp.DrawLine(pen, X + radius, Y, X + (int)gridsize - radius, Y);
         }
         private void DrawRightLine(Pen pen, Graphics gp, double gridsize, int X, int Y, int radius)
         {
@@ -68,49 +66,53 @@ namespace DrawSubRegionPolyomino
         }
         private void DrawLmidLine(Pen pen, Graphics gp, double gridsize, int X, int Y, int radius)
         {
-            gp.DrawLine(pen, X, Y+radius, X, Y +(int)gridsize - radius);
+            gp.DrawLine(pen, X, Y + radius, X, Y + (int)gridsize - radius);
         }
         private void DrawDownLine(Pen pen, Graphics gp, double gridsize, int X, int Y, int radius)
         {
-            gp.DrawLine(pen, X, Y + (int)gridsize - radius, X, Y+(int)gridsize);
+            gp.DrawLine(pen, X, Y + (int)gridsize - radius, X, Y + (int)gridsize);
         }
         private void DrawULCircle(Pen pen, Graphics gp, double gridsize, int X, int Y, int radius)
         {
-            gp.DrawArc(pen, X, Y, 2*radius,2*radius,180,90);
+            gp.DrawArc(pen, X, Y, 2 * radius, 2 * radius, 180, 90);
         }
         private void DrawURCircle(Pen pen, Graphics gp, double gridsize, int X, int Y, int radius)
         {
-            gp.DrawArc(pen, X+(int)gridsize-2*radius, Y, 2 * radius, 2 * radius, 270, 90);
+            gp.DrawArc(pen, X + (int)gridsize - 2 * radius, Y, 2 * radius, 2 * radius, 270, 90);
         }
         private void DrawDLCircle(Pen pen, Graphics gp, double gridsize, int X, int Y, int radius)
         {
-            gp.DrawArc(pen, X, Y+(int)gridsize-2*radius, 2 * radius, 2 * radius, 90, 90);
+            gp.DrawArc(pen, X, Y + (int)gridsize - 2 * radius, 2 * radius, 2 * radius, 90, 90);
         }
         private void DrawDRCircle(Pen pen, Graphics gp, double gridsize, int X, int Y, int radius)
         {
-            gp.DrawArc(pen, X + (int)gridsize - 2*radius, Y + (int)gridsize - 2*radius, 2 * radius, 2 * radius, 0, 90);
+            gp.DrawArc(pen, X + (int)gridsize - 2 * radius, Y + (int)gridsize - 2 * radius, 2 * radius, 2 * radius, 0, 90);
         }
-
-        private void DrawPoly(Pen pen, Graphics gp,double gridsize,Point[] pts,string[] group,double roundRatio)
+        struct Piece
         {
-            for(int i=0;i<pts.Length;i++)
-            {
-                Point p = pts[i];
-                int X = (int)(p.X * gridsize);
-                int Y = (int)(p.Y * gridsize);
-                int radius = (int)(gridsize / 2 * roundRatio);
-                DrawULCircle(pen, gp, gridsize, X, Y, radius);
-                DrawURCircle(pen, gp, gridsize, X, Y, radius);
-                DrawDLCircle(pen, gp, gridsize, X, Y, radius);
-                DrawDRCircle(pen, gp, gridsize, X, Y, radius);
-                DrawLeftLine(pen, gp, gridsize, X, Y, radius);
-                DrawMidLine(pen, gp, gridsize, X, Y, radius);
-                DrawRightLine(pen, gp, gridsize, X, Y, radius);
-                DrawUpLine(pen, gp, gridsize, X, Y, radius);
-                DrawLmidLine(pen, gp, gridsize, X, Y, radius);
-                DrawDownLine(pen, gp, gridsize, X, Y, radius);
-            }
-           
+            public string groupNUm;
+        }
+        private void DrawPoly(Pen pen, Graphics gp, double gridsize, Piece[,] Pieces, double roundRatio)
+        {
+            for (int i = 0; i < Pieces.GetLength(0); i++)
+                for (int j = 0; j < Pieces.GetLength(1); j++)
+                    if (Pieces[i, j].groupNUm != "0")
+                    {
+                        int X = (int)(i * gridsize);
+                        int Y = (int)(j * gridsize);
+                        int radius = (int)(gridsize / 2 * roundRatio);
+                        DrawULCircle(pen, gp, gridsize, X, Y, radius);
+                        DrawURCircle(pen, gp, gridsize, X, Y, radius);
+                        DrawDLCircle(pen, gp, gridsize, X, Y, radius);
+                        DrawDRCircle(pen, gp, gridsize, X, Y, radius);
+                        DrawLeftLine(pen, gp, gridsize, X, Y, radius);
+                        DrawMidLine(pen, gp, gridsize, X, Y, radius);
+                        DrawRightLine(pen, gp, gridsize, X, Y, radius);
+                        DrawUpLine(pen, gp, gridsize, X, Y, radius);
+                        DrawLmidLine(pen, gp, gridsize, X, Y, radius);
+                        DrawDownLine(pen, gp, gridsize, X, Y, radius);
+                    }
+
         }
 
         private void refresh_Click(object sender, EventArgs e)
